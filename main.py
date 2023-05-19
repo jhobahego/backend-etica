cac85e18ba484de791c228c8d10d5f87e25106c2
Jhon Hernandez <jhobahego16@hotmail.com>
2023-05-19 16:01:03 -0500

    Feat: cors para producción y desarrollo

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from routes.usuarios import usuario
 
 app = FastAPI()
 
 app.include_router(usuario)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "https://6467d957af410b0cb2767d0e--taupe-melba-0df3aa.netlify.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
