deb8ad883d8a00fab5c81141af08446ddc8551f0
Jhon Hernandez <jhobahego16@hotmail.com>
2023-05-19 20:12:32 -0500

    Fix: cors actualizados por nueva url de producción

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app.include_router(usuario)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://6467d957af410b0cb2767d0e--taupe-melba-0df3aa.netlify.app"],
+    allow_origins=["http://localhost:5173", "https://programa-contable.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
