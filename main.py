25f8f87e86020d8cbd5cb07a2479bd69d77b37d7
Jhon Hernandez <jhobahego16@hotmail.com>
2025-04-13 19:43:49 -0500

    Refactor: Origin de cors desde variable de entorno

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
+from decouple import config
 
 from routes.usuarios import usuario
 
@@ -8,13 +9,11 @@ app = FastAPI()
 
 app.include_router(usuario)
 
+FRONTEND_URL = config("FRONTEND_URL")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://programa-contable.netlify.app",
-        "https://main--programa-contable.netlify.app",
-    ],
+    allow_origins=[FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
